4b7ff282ce55a90831c5ecf1e896f2178165e818
soumi guria <soumiguria.cse2022@nsec.ac.in>
2025-06-26 15:36:40 +0530

    prebuild backend from another project

---
+++ backend/createAdmin.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Admin = require("./models/Admin");
+
+mongoose
+  .connect(
+    "mongodb+srv://guriasoumi29:soumi1234@cluster0.bsahqpz.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+async function createAdmin() {
+  const admin = new Admin({
+    firstName: "abc",
+    lastName: "xyz",
+    email: "abc@gmail.com",
+    password: "xyz123",
+    role: "admin",
+  });
+
+  try {
+    await admin.save();
+    console.log("Admin created successfully");
+  } catch (error) {
+    console.error("Error creating admin:", error);
+  } finally {
+    mongoose.connection.close();
+  }
+}
+
+createAdmin();
